22e4ae0e85daadf20f07f07d54701236c9bea4f7
Michael Liao <askxuefeng@gmail.com>
2015-12-19 09:09:54 -0800

    add http samples

---
+++ samples/node/http/file_server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// a simple http server
+
+var
+    fs = require('fs'),
+    url = require('url'),
+    path = require('path'),
+    http = require('http');
+
+var root = path.resolve(process.argv[2] || '.');
+
+console.log('Static root dir: ' + root);
+
+var server = http.createServer(function (request, response) {
+    var
+        pathname = url.parse(request.url).pathname, // '/static/bootstrap.css'
+        filepath = path.join(root, pathname); // '/srv/www/static/bootstrap.css'
+    fs.stat(filepath, function (err, stats) {
+        if (!err && stats.isFile()) {
+            console.log('200 ' + request.url);
+            response.writeHead(200);
+            fs.createReadStream(filepath).pipe(response);
+        } else {
+            console.log('404 ' + request.url);
+            response.writeHead(404);
+            response.end('404 Not Found');
+        }
+    });
+});
+
+server.listen(8080);
+
+console.log('Server is running at http://127.0.0.1:8080/');
---
+++ samples/node/http/hello.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// a simple http server that always display 'Hello, world!'
+
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<h1>Hello world!</h1>');
+});
+
+server.listen(8080);
+
+console.log('Server is running at http://127.0.0.1:8080/');
