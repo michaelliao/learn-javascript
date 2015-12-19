84606411a02043948fea2aabdbfed13f1aea187b
Michael Liao <askxuefeng@gmail.com>
2015-12-19 09:07:43 -0800

    add timers samples

---
+++ samples/node/timers/timers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('Now: ' + new Date().toTimeString());
+
+setTimeout(function () {
+    console.log('Run at ' + new Date().toTimeString());
+}, 1500);
+
+setInterval(function () {
+    console.log('Schedule at ' + new Date().toTimeString());
+}, 3000);
