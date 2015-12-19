edf0f82f2c1d46322fec017fa9baaa1898623b31
Michael Liao <askxuefeng@gmail.com>
2015-12-19 09:07:32 -0800

    add os samples

---
+++ samples/node/os/get_os_info.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var os = require('os');
+
+console.log('CPU: ' + JSON.stringify(os.cpus()));
+
+console.log('Network: ' + JSON.stringify(os.networkInterfaces()));
+
+console.log('Total memory: ' + os.totalmem());
+
+console.log('Free memory: ' + os.freemem());
+
+console.log('Hostname: ' + os.hostname());
+
+console.log('Platform: ' + os.platform());
+
+console.log('Temp dir: ' + os.tmpdir());
+
+console.log('OS type: ' + os.type());
+
+console.log('Uptime: ' + os.uptime());
