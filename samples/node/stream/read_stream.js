cb7e2480d8fd1598d82ae053d53aa738ff270322
Michael Liao <askxuefeng@gmail.com>
2015-12-20 16:43:41 -0800

    update read stream

--- samples/node/stream/read_stream.js
+++ samples/node/stream/read_stream.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 var rs = fs.createReadStream('sample.txt', 'utf-8');
 rs.on('data', function (chunk) {
+    console.log('DATA:')
     console.log(chunk);
 });
 rs.on('end', function () {
