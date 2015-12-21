3dc1b4a60895a7f9bcb2f71a1b6c71986ef6eb41
Michael Liao <askxuefeng@gmail.com>
2015-12-21 08:53:13 -0800

    add log

--- samples/node/http/hello.js
+++ samples/node/http/hello.js
@@ -5,6 +5,7 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
+    console.log(request.method + ': ' + request.url);
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('<h1>Hello world!</h1>');
 });
