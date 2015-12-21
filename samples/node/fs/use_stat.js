364056947e276e0dbf2755d2b5bb3c3ca74c9bc4
Michael Liao <askxuefeng@gmail.com>
2015-12-20 16:26:39 -0800

    add stat sample

---
+++ samples/node/fs/use_stat.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fs = require('fs');
+
+fs.stat('sample.txt', function (err, stat) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('isFile: ' + stat.isFile());
+        console.log('isDirectory: ' + stat.isDirectory());
+        if (stat.isFile()) {
+            console.log('size: ' + stat.size);
+            console.log('birth time: ' + stat.birthtime);
+            console.log('modified time: ' + stat.mtime);
+        }
+    }
+});
