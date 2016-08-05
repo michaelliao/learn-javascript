c9eded613cc7b127706ee68edfad79810b7f23d9
Michael Liao <askxuefeng@gmail.com>
2016-08-05 21:45:55 +0800

    add vscode support

--- samples/node/global/gl.js
+++ samples/node/global/gl.js
@@ -14,8 +14,13 @@ console.log('arguments: ' + JSON.stringify(process.argv));
 // process.cwd() 返回当前工作目录:
 console.log('cwd: ' + process.cwd());
 
-// 切换当前工作目录, Windows的目录类似 'C:\\Windows\\System32'
-process.chdir('/private/tmp');
+// 切换当前工作目录:
+var d = '/private/tmp';
+if (process.platform === 'win32') {
+    // 如果是Windows，切换到 C:\Windows\System32
+    d = 'C:\\Windows\\System32';
+}
+process.chdir(d);
 console.log('cwd: ' + process.cwd());
 
 // process.nextTick()将在下一轮事件循环中调用:
