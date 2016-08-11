fa52a98a8e872306cd57026debd9260f469e62de
Michael Liao <askxuefeng@gmail.com>
2016-08-11 20:45:03 +0800

    add source for hello-koa

---
+++ samples/node/web/koa/hello-koa/app.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime();
+    await next();
+    const ms = new Date().getTime() - start;
+    console.log(`${ctx.request.method} ${ctx.request.url}: ${ms}ms`);
+    ctx.response.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+app.listen(3000);
+console.log('app started at port 3000...');
---
+++ samples/node/web/koa/hello-koa/start.js
@@ -0,0 +1,5 @@
+require('babel-core/register')({
+    presets: ['es2015-node6', 'stage-3']
+});
+
+require('./app.js');
