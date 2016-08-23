3de6ae0dda3b4774cdc1b2bdcf7edfc3a29d20a4
Michael Liao <askxuefeng@gmail.com>
2016-08-23 22:08:48 +0800

    add hello-sequelize

---
+++ samples/node/web/db/hello-sequelize/app.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+
+const config = require('./config');
+
+console.log('init sequelize...');
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+var Pet = sequelize.define('pet', {
+    id: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+}, {
+        timestamps: false
+    });
+
+var now = Date.now();
+
+Pet.create({
+    id: 'g-' + now,
+    name: 'Gaffey',
+    gender: false,
+    birth: '2007-07-07',
+    createdAt: now,
+    updatedAt: now,
+    version: 0
+}).then(function (p) {
+    console.log('created.' + JSON.stringify(p));
+}).catch(function (err) {
+    console.log('failed: ' + err);
+});
+
+(async () => {
+    var dog = await Pet.create({
+        id: 'd-' + now,
+        name: 'Odie',
+        gender: false,
+        birth: '2008-08-08',
+        createdAt: now,
+        updatedAt: now,
+        version: 0
+    });
+    console.log('created: ' + JSON.stringify(dog));
+})();
+
+(async () => {
+    var pets = await Pet.findAll({
+        where: {
+            name: 'Gaffey'
+        }
+    });
+    console.log(`find ${pets.length} pets:`);
+    for (let p of pets) {
+        console.log(JSON.stringify(p));
+        console.log('update pet...');
+        p.gender = true;
+        p.updatedAt = Date.now();
+        p.version ++;
+        await p.save();
+        if (p.version === 3) {
+            await p.destroy();
+            console.log(`${p.name} was destroyed.`);
+        }
+    }
+})();
---
+++ samples/node/web/db/hello-sequelize/config.js
@@ -0,0 +1,10 @@
+
+var config = {
+    database: 'test',
+    username: 'www',
+    password: 'www',
+    host: 'localhost',
+    port: 3306
+};
+
+module.exports = config;
