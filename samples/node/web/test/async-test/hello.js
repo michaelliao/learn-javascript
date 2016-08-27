0f7073d3203bccd575b24b7fbef4969d66c4baa3
Michael Liao <askxuefeng@gmail.com>
2016-08-27 21:07:33 +0800

    add async test

---
+++ samples/node/web/test/async-test/hello.js
@@ -0,0 +1,13 @@
+// hello.js
+console.log('init hello.js...');
+
+const fs = require('mz/fs');
+
+// a simple async function:
+module.exports = async () => {
+    let expression = await fs.readFile('./data.txt', 'utf-8');
+    let fn = new Function('return ' + expression);
+    let r = fn();
+    console.log(`Calculate: ${expression} = ${r}`);
+    return r;
+};
---
+++ samples/node/web/test/async-test/test/await-test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+const hello = require('../hello');
+
+describe('#async hello', () => {
+    describe('#asyncCalculate()', () => {
+        // function(done) {}
+        it('#async with done', (done) => {
+            (async function () {
+                try {
+                    let r = await hello();
+                    assert.strictEqual(r, 15);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            })();
+        });
+
+        it('#async function', async () => {
+            let r = await hello();
+            assert.strictEqual(r, 15);
+        });
+
+        it('#sync function', () => {
+            assert(true);
+        });
+    });
+});
