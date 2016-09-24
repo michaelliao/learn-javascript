b0be51bb5996d3754536a3c1f5c543cfd5b3c7e6
Michael Liao <askxuefeng@gmail.com>
2016-09-24 10:38:27 +0800

    update app.js

--- samples/node/web/koa/view-koa/app.js
+++ samples/node/web/koa/view-koa/app.js
@@ -2,11 +2,9 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
-const router = require('koa-router')();
+const controller = require('./controller');
 
-const templating = require('./templating.js');
-
-const fs = require('fs');
+const templating = require('./templating');
 
 const app = new Koa();
 
@@ -38,41 +36,8 @@ app.use(templating('view', {
     watch: !isProduction
 }));
 
-// add url-route in /controllers:
-
-function addMapping(router, mapping) {
-    for (var url in mapping) {
-        if (url.startsWith('GET ')) {
-            var path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if (url.startsWith('POST ')) {
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-function addControllers(router) {
-    var files = fs.readdirSync(__dirname + '/controllers');
-    var js_files = files.filter((f) => {
-        return f.endsWith('.js');
-    }, files);
-
-    for (var f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
-        addMapping(router, mapping);
-    }
-}
-
-addControllers(router);
-
-// add router middleware:
-app.use(router.routes());
+// add controller:
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
--- samples/node/web/rest/rest-koa/app.js
+++ samples/node/web/rest/rest-koa/app.js
@@ -2,11 +2,9 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
-const router = require('koa-router')();
+const controller = require('./controller');
 
-const fs = require('fs');
-
-const templating = require('./templating.js');
+const templating = require('./templating');
 
 const rest = require('./rest');
 
@@ -34,49 +32,8 @@ app.use(templating('view', {
 // bind .rest() for ctx:
 app.use(rest.restify());
 
-// add url-route in /controllers:
-
-function addMapping(router, mapping) {
-    for (var url in mapping) {
-        if (url.startsWith('GET ')) {
-            var path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if (url.startsWith('POST ')) {
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else if (url.startsWith('PUT ')) {
-            var path = url.substring(4);
-            router.put(path, mapping[url]);
-            console.log(`register URL mapping: PUT ${path}`);
-        } else if (url.startsWith('DELETE ')) {
-            var path = url.substring(7);
-            router.del(path, mapping[url]);
-            console.log(`register URL mapping: DELETE ${path}`);
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-function addControllers(router) {
-    var files = fs.readdirSync(__dirname + '/controllers');
-    var js_files = files.filter((f) => {
-        return f.endsWith('.js');
-    }, files);
-
-    for (var f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
-        addMapping(router, mapping);
-    }
-}
-
-addControllers(router);
-
-// add router middleware:
-app.use(router.routes());
+// add controllers:
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
