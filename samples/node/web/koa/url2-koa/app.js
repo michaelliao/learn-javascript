386b37a89f9db9f4e8c54420a081333b73465dfa
Michael Liao <askxuefeng@gmail.com>
2016-09-24 10:26:41 +0800

    refactor url2-koa

--- samples/node/web/koa/url2-koa/app.js
+++ samples/node/web/koa/url2-koa/app.js
@@ -2,9 +2,7 @@ const Koa = require('koa');
 
 const bodyParser = require('koa-bodyparser');
 
-const router = require('koa-router')();
-
-const fs = require('fs');
+const controller = require('./controller');
 
 const app = new Koa();
 
@@ -17,41 +15,8 @@ app.use(async (ctx, next) => {
 // parse request body:
 app.use(bodyParser());
 
-// add url-route in /controllers:
-
-function addMapping(router, mapping) {
-    for (var url in mapping) {
-        if (url.startsWith('GET ')) {
-            var path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if (url.startsWith('POST ')) {
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-function addControllers(router) {
-    var files = fs.readdirSync(__dirname + '/controllers');
-    var js_files = files.filter((f) => {
-        return f.endsWith('.js');
-    }, files);
-
-    for (var f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
-        addMapping(router, mapping);
-    }
-}
-
-addControllers(router);
-
-// add router middleware:
-app.use(router.routes());
+// add controllers:
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
